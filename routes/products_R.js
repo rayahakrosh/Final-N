5529005849e6fd776403159e7e49c3decbd6bc41
rayahakrosh <rraya879@gmail.com>
2025-10-03 15:29:58 +0300

    התחלת routes

---
+++ routes/products_R.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs'); 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images/'); 
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage }).single('projectImage'); 
+
+let projects = [
+    { 
+        id: uuidv4(), 
+        name: "FinalProject", 
+        description: "World!", 
+        imageUrl: "images/default-proj.jpg", 
+        rating: 4 
+    }
+];
+
+const validateProject = (data) => {
+    if (!data.name || data.name.length < 3) return "Project Name must be at least 3 characters long.";
+    if (!data.description || data.description.length < 10) return "Description must be at least 10 characters long.";
+    return null; 
+};
